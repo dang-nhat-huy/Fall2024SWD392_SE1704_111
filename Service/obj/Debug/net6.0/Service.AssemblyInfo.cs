47d9a37b3f776896557dc2fa2fcac362cf2c3646
Dang Nhat Huy <113001292+dang-nhat-huy@users.noreply.github.com>
2024-09-29 14:08:04 +0700

    Revert "test"
    This reverts commit ad2a4bd1c5a7b75a1587dad89c3efe3c91794a49.

--- Service/obj/Debug/net6.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net6.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0cf8e6d8c49076bfe8b69ef7ce4ce74362fd54e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea2c2583192e7866599892af9963de350efcd3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
