3f5e66c60b9d1b47d25d747e1f77e08f911daa7a
NhatTruongFPT <truongtnse140532@fpt.edu.vn>
2024-10-15 20:15:02 +0700

    Add razor FE + ReportFE

---
+++ Fall2024_SWD392_SE1704_111_FE/Pages/ReportFE/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject.Model;
+
+namespace Fall2024_SWD392_SE1704_111_FE.Pages.ReportFE
+{
+    public class EditModel : PageModel
+    {
+        private readonly BusinessObject.Model.HairSalonBookingContext _context;
+
+        public EditModel(BusinessObject.Model.HairSalonBookingContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Report Report { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Reports == null)
+            {
+                return NotFound();
+            }
+
+            var report =  await _context.Reports.FirstOrDefaultAsync(m => m.ReportId == id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+            Report = report;
+           ViewData["BookingId"] = new SelectList(_context.Bookings, "BookingId", "BookingId");
+           ViewData["StylistId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Report).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReportExists(Report.ReportId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ReportExists(int id)
+        {
+          return (_context.Reports?.Any(e => e.ReportId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Fall2024_SWD392_SE1704_111_FE/Pages/ReportFE/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject.Model;
+
+namespace Fall2024_SWD392_SE1704_111_FE.Pages.ReportFE
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BusinessObject.Model.HairSalonBookingContext _context;
+
+        public IndexModel(BusinessObject.Model.HairSalonBookingContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Report> Report { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Reports != null)
+            {
+                Report = await _context.Reports
+                .Include(r => r.Booking)
+                .Include(r => r.Stylist).ToListAsync();
+            }
+        }
+    }
+}
